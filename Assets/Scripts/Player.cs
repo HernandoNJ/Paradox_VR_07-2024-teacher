d71587f904c13882a64832659f1de35bb0cfeb5e
Hernando Nieto <hernandonj.unity@gmail.com>
2024-07-15 22:07:53 -0500

    Initial files added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+	// int: 1,2,3... -1, -2, -3...
+	// float: 1,3   4,8   3   5 
+    public int numeroDeLlantas;
+    public int numeroDePuertas;
+    public float cantidadDeGasolina;
+
+	private void Start()
+	{
+		//numeroDeLlantas = 4;
+		//numeroDePuertas = 5;
+		//cantidadDeGasolina = 1.5f;
+	}
+
+
+	//IDE VISUAL STUDIO
+	//C#
+
+
+	//// Start is called before the first frame update
+	//void Start()
+	//{
+	//    //Change color to orange
+	//}
+
+	//// Update is called once per frame
+	//void Update()
+	//{
+
+	//}
+}
